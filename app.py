aba9e1bb38af96ee38d856f06d51f5d297c44a82
Michael Jones <mjones@kotrs.com>
2021-09-21 00:02:48 -0400

    First commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+def _sum(arr):
+    sum=0
+
+    for val in arr:
+        sum = sum + val
+
+    return(sum)
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate_value():
+    return { 'calculatedValue': _sum(request.get_json()) }
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
